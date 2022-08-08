a8e3325fdc03ce724e043ab9f30d652291a3aa92
larnee <larnee@mailfence.com>
2022-08-08 16:40:12 +0200

    Solution 1

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// your first API endpoint...
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: req.ip, language: req.language, software: req.header('user-agent') });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
