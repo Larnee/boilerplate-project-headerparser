e78511ec5bf966b7b8c4e711a67ede4c1b1d02a7
larnee <larnee@mailfence.com>
2022-08-08 16:54:31 +0200

    Solution 3

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-  res.json({ ipaddress: req.ip, language: req.acceptsLanguages, software: req.userAgent });
+  res.json({ ipaddress: req.ip, language: req.header("accept-language"), software: req.header('user-agent') });
 });
 
 // listen for requests :)
